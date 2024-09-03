d98e3f7a315ad4b5918c33b1eab66800478be483
Victor <vimariano05@hotmail.com>
2024-09-02 21:02:07 -0300

    mensagem

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    <?php
+        $name=2;// { }
+        echo $name;
+        if($name==2){
+            echo $name+1;
+         }
+?>
+    </body>
+</html>
+<?php
+
+?>
